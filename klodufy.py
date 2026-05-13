fa8e3e50f9123add042aa0363ef1a5fe8a49fe17
Alex Andrix <alex@alexandrix.com>
2026-05-13 22:04:14 +0900

    Add Emma Aycoberyy's pressure box to klodufy

--- klodufy.py
+++ klodufy.py
@@ -669,5 +669,26 @@ def klodufy_yohandubois_galaxy_bz ():
     nb_logs = 20
     skip_scanning = False
     
-    klodufy (source_file, file_type_token, size, dimensions, minmaxs, quality, dest_path, dest_file_name, testing_density, nb_logs, skip_scanning)
+    klodufy(source_file, file_type_token, size, dimensions, minmaxs, quality, dest_path, dest_file_name, testing_density, nb_logs, skip_scanning)
 # klodufy_yohandubois_galaxy_bz()
+
+# EMMAAYCOBERRY BOX
+def klodufy_emmaaycoberry_box ():
+
+    dimensions = [ ["rho", "log"] ]
+    minmaxs = [ [-11, 1] ]
+    file_prefix = "density"
+
+    source_file = "./data/emmaaycoberry/1-frame/pressure_map.dat"
+    file_type_token = "DAT"
+    size = 512
+    quality = "high"
+    dest_path = "emmaaycoberry/1-frame/"
+    dest_file_name = "emmaaycoberry-box-rho-" + str(size)
+    testing_density = 1/2 # 1/1 is full rendering
+    nb_logs = 20
+    skip_scanning = False
+    
+    klodufy(source_file, file_type_token, size, dimensions, minmaxs, quality, dest_path, dest_file_name, testing_density, nb_logs, skip_scanning)
+
+klodufy_emmaaycoberry_box ()
