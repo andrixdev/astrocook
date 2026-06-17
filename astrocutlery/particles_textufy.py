d12d728b11c3c03a3590454a884b63f535b6100a
Alex Andrix <alex@alexandrix.com>
2026-06-17 13:17:15 +0200

    Extract scan and export functions of particles_textufy into their own methods

--- astrocutlery/particles_textufy.py
+++ astrocutlery/particles_textufy.py
@@ -146,6 +146,172 @@ def prepare_particles_data(source_file, file_type_token):
 		
 		return False
 
+def particles_scan(data, actual_count, step, dimensions, file_type_token, nb_logs):
+	logger.info("Scanning data to detect extrema for remapping...")
+	start_time = datetime.datetime.now()
+	
+	# Init scanned minmax array (extremal values of positions, velocities... whatever)
+	real_minmaxs = []
+	dims = len(dimensions)
+	for d in range(0, dims):
+		real_minmaxs.append([float("inf"), float("-inf")])
+	
+	# Start loop
+	ii = 0
+	for i in range(0, actual_count):
+		ii = i * step
+		
+		row = ""
+		
+		for d in range(0, dims):
+			dimension_name = dimensions[d][0]
+			dimension_mode = dimensions[d][1]
+			
+			# Grab data value Shamrock/Phantom way (dimension name)
+			if (file_type_token == "SHAMROCK"):
+				# Special case for Yona's rho, derived from hpart
+				if (dimension_name == "rho"):
+					val = 1 * (data.iloc[ii]["hpart"] ** 3)
+				else:
+					val = data.iloc[ii][dimension_name]
+			
+			elif (file_type_token == "PHANTOM"):
+				val = data.iloc[ii][dimension_name]
+				
+			# Grab data value basic way (just the order)
+			elif (file_type_token == "NUMPY" or file_type_token == "TXT" or file_type_token == "HDF5" or file_type_token == "HDF5-SANHAN" or file_type_token == "SAV" or file_type_token == "HDF5-GOY"):
+				val = data[ii][d]
+				
+			# Checking mode
+			if (dimension_mode == "log"):
+				val = math.log10(val)
+				
+			# Rounding (5 digits just for the scan)
+			val = round_to_n(val, 5)
+			
+			# Feed row to potentially print
+			if (d > 0):
+				row = row + " "
+			row = row + str(val)
+			
+			# Update max value
+			if (val > real_minmaxs[d][1]):
+				real_minmaxs[d][1] = val
+				
+			# Update min value
+			if (val < real_minmaxs[d][0]):
+				real_minmaxs[d][0] = val
+			
+		if (i % max(1, int(round(actual_count/nb_logs))) == 0):
+			logger.bind(color="fg #E44").trace(str(i) + "th row is: " + row)
+		
+	# Log detected extrema
+	for d in range(0, dims):
+		dimension_name = dimensions[d][0]
+		logger.bind(color="fg #DA7").trace("Min value for " + dimension_name + " is: " + str(real_minmaxs[d][0]))
+		logger.bind(color="fg #DA7").trace("Max value for " + dimension_name + " is: " + str(real_minmaxs[d][1]))
+		
+	# Log scanning time
+	end_time = datetime.datetime.now()
+	delta = end_time.timestamp() - start_time.timestamp()
+	logger.success("Scanned data in: " + str(round(delta, 2)) + " seconds.")
+
+def particles_export(data, actual_count, step, dimensions, kept_dimensions, minmaxs, file_type_token, nb_logs, destination_file, dest_file_name, zoombox=False):
+	# Log start info
+	logger.info("Exporting: remapping data and writing to file...")
+	start_time = datetime.datetime.now()
+	if (zoombox):
+		logger.warning("Filtering with zoombox: " + str(zoombox) + " (x, y, z, rad)")
+
+	# Init
+	is_first_line_written = True
+	is_in_box = True # Init to true to avoid issues when zoombox is not set
+	output_rows_count = 0
+
+	# Loop
+	dims = len(dimensions)
+	for j in range(0, actual_count):
+		jj = j * step
+		
+		row = ""
+		
+		# Prepare remap
+		low_quality_digits = 3
+		high_quality_digits = 6
+		lq_max = 10 ** low_quality_digits
+		hq_max = 10 ** high_quality_digits
+		
+		# Prepare boolean to check if inside the zoomed box
+		if (zoombox):
+			is_in_box = is_within_box(data[jj][0], data[jj][1], data[jj][2], zoombox[0], zoombox[1], zoombox[2], zoombox[3])
+
+		if (not is_first_line_written):
+			row += "\n"
+
+		for d in range(0, dims):
+			dimension_name = dimensions[d][0]
+			dimension_mode = dimensions[d][1]
+			dimension_quality = dimensions[d][2]
+			is_dimension_kept = True if (kept_dimensions[d] == 1) else False
+			
+			digits = low_quality_digits if (dimension_quality == "LQ") else high_quality_digits
+			
+			# Grab data value Shamrock/Phantom way (dimension name)
+			if (file_type_token == "SHAMROCK"):
+				# Special case for Yona's rho, derived from hpart
+				if (dimension_name == "rho"):
+					val = 1 * (data.iloc[jj]["hpart"] ** 3)
+				else:
+					val = data.iloc[jj][dimension_name]                   
+
+			elif (file_type_token == "PHANTOM"):
+				val = data.iloc[jj][dimension_name]
+				
+			# Grab data value basic way (just the order)
+			elif (file_type_token == "NUMPY" or file_type_token == "TXT" or file_type_token == "HDF5" or file_type_token == "HDF5-SANHAN" or file_type_token == "SAV" or file_type_token == "HDF5-GOY"):
+				val = data[jj][d]
+
+			# Checking mode
+			if (dimension_mode == "log"):
+				val = math.log10(val)
+			
+			# Remap
+			min_val = minmaxs[d][0]
+			max_val = minmaxs[d][1]
+			min_target = 0
+			max_target = lq_max if (dimension_quality == "LQ") else hq_max
+			val = int(round_to_n(remap(val, min_val, max_val, min_target, max_target, True), digits + 1))
+			
+			# Feed row to later write to file
+			if (is_dimension_kept):
+				if (d > 0):
+					row = row + " "
+				
+				if (not zoombox or (zoombox and is_in_box)):
+					row = row + str(val)
+					
+		# Log row sometimes
+		if (j % max(1, int(round(actual_count/nb_logs))) == 0):
+			if (zoombox and not is_in_box):
+				logger.bind(color="fg #3FB").trace(str(j) + "th remapped row is: out of zoombox")
+			else:
+				logger.bind(color="fg #3FB").trace(str(j) + "th remapped row is: " + row.lstrip('\n'))
+
+		# Write to file
+		if (not zoombox or (zoombox and is_in_box)):
+			destination_file.write(row)
+			output_rows_count += 1
+			is_first_line_written = False
+
+	# Log export time
+	end_time = datetime.datetime.now()
+	delta = end_time.timestamp() - start_time.timestamp()
+	logger.success("Exported data in: " + str(round(delta, 2)) + " seconds.")
+
+	# Conclude
+	logger.success("File " + dest_file_name + ".txt with " + str(output_rows_count) + " rows was created.")
+
+
 # Main function to textufy particles data dumps, with options to customize the process
 def particles_textufy (source_file, file_type_token, dest_path, dest_file_name, dimensions, kept_dimensions, minmaxs, testing_density, nb_logs, is_scanning, is_exporting, zoombox=None):
 
@@ -172,8 +338,7 @@ def particles_textufy (source_file, file_type_token, dest_path, dest_file_name, 
 	# Prepare export file
 	destination_file = open("output/" + dest_path + dest_file_name + ".txt", "w")
 	
-	# Get dimensions and log ratio
-	dims = len(dimensions)
+	# Get log ratio
 	count = data.shape[0]
 	actual_count = math.floor(count * testing_density)
 	log_ratio = "all of " if testing_value == 1 else ("1 in " + str(testing_value) + " of all ")
@@ -181,166 +346,10 @@ def particles_textufy (source_file, file_type_token, dest_path, dest_file_name, 
 	
 	# LOOP 1: scan
 	if (is_scanning):
-		logger.info("Scanning data to detect extrema for remapping...")
-		start_time = datetime.datetime.now()
-		
-		# Init scanned minmax array (extremal values of positions, velocities... whatever)
-		real_minmaxs = []
-		for d in range(0, dims):
-			real_minmaxs.append([float("inf"), float("-inf")])
-		
-		# Start loop
-		ii = 0
-		for i in range(0, actual_count):
-			ii = i * step
-			
-			row = ""
-			
-			for d in range(0, dims):
-				dimension_name = dimensions[d][0]
-				dimension_mode = dimensions[d][1]
-				
-				# Grab data value Shamrock/Phantom way (dimension name)
-				if (file_type_token == "SHAMROCK"):
-					# Special case for Yona's rho, derived from hpart
-					if (dimension_name == "rho"):
-						val = 1 * (data.iloc[ii]["hpart"] ** 3)
-					else:
-						val = data.iloc[ii][dimension_name]
-				
-				elif (file_type_token == "PHANTOM"):
-					val = data.iloc[ii][dimension_name]
-					
-				# Grab data value basic way (just the order)
-				elif (file_type_token == "NUMPY" or file_type_token == "TXT" or file_type_token == "HDF5" or file_type_token == "HDF5-SANHAN" or file_type_token == "SAV" or file_type_token == "HDF5-GOY"):
-					val = data[ii][d]
-					
-				# Checking mode
-				if (dimension_mode == "log"):
-					val = math.log10(val)
-					
-				# Rounding (5 digits just for the scan)
-				val = round_to_n(val, 5)
-				
-				# Feed row to potentially print
-				if (d > 0):
-					row = row + " "
-				row = row + str(val)
-				
-				# Update max value
-				if (val > real_minmaxs[d][1]):
-					real_minmaxs[d][1] = val
-					
-				# Update min value
-				if (val < real_minmaxs[d][0]):
-					real_minmaxs[d][0] = val
-				
-			if (i % max(1, int(round(actual_count/nb_logs))) == 0):
-				logger.bind(color="fg #E44").trace(str(i) + "th row is: " + row)
-			
-		# Log detected extrema
-		for d in range(0, dims):
-			dimension_name = dimensions[d][0]
-			logger.bind(color="fg #DA7").trace("Min value for " + dimension_name + " is: " + str(real_minmaxs[d][0]))
-			logger.bind(color="fg #DA7").trace("Max value for " + dimension_name + " is: " + str(real_minmaxs[d][1]))
-			
-		# Log scanning time
-		end_time = datetime.datetime.now()
-		delta = end_time.timestamp() - start_time.timestamp()
-		logger.success("Scanned data in: " + str(round(delta, 2)) + " seconds.")
+		particles_scan(data, actual_count, step, dimensions, file_type_token, nb_logs)
 	
 	# LOOP 2: export (remap & write)
 	if (is_exporting):
+		particles_export(data, actual_count, step, dimensions, kept_dimensions, minmaxs, file_type_token, nb_logs, destination_file, dest_file_name, zoombox=False)
 
-		# Log start info
-		logger.info("Exporting: remapping data and writing to file...")
-		start_time = datetime.datetime.now()
-		if (zoombox):
-			logger.warning("Filtering with zoombox: " + str(zoombox) + " (x, y, z, rad)")
-
-		# Init
-		is_first_line_written = True
-		is_in_box = True # Init to true to avoid issues when zoombox is not set
-		output_rows_count = 0
-
-		# Loop
-		for j in range(0, actual_count):
-			jj = j * step
-			
-			row = ""
-			
-			# Prepare remap
-			low_quality_digits = 3
-			high_quality_digits = 6
-			lq_max = 10 ** low_quality_digits
-			hq_max = 10 ** high_quality_digits
-			
-			# Prepare boolean to check if inside the zoomed box
-			if (zoombox):
-				is_in_box = is_within_box(data[jj][0], data[jj][1], data[jj][2], zoombox[0], zoombox[1], zoombox[2], zoombox[3])
-
-			if (not is_first_line_written):
-				row += "\n"
-
-			for d in range(0, dims):
-				dimension_name = dimensions[d][0]
-				dimension_mode = dimensions[d][1]
-				dimension_quality = dimensions[d][2]
-				is_dimension_kept = True if (kept_dimensions[d] == 1) else False
-				
-				digits = low_quality_digits if (dimension_quality == "LQ") else high_quality_digits
-				
-				# Grab data value Shamrock/Phantom way (dimension name)
-				if (file_type_token == "SHAMROCK"):
-					# Special case for Yona's rho, derived from hpart
-					if (dimension_name == "rho"):
-						val = 1 * (data.iloc[jj]["hpart"] ** 3)
-					else:
-						val = data.iloc[jj][dimension_name]                   
-
-				elif (file_type_token == "PHANTOM"):
-					val = data.iloc[jj][dimension_name]
-					
-				# Grab data value basic way (just the order)
-				elif (file_type_token == "NUMPY" or file_type_token == "TXT" or file_type_token == "HDF5" or file_type_token == "HDF5-SANHAN" or file_type_token == "SAV" or file_type_token == "HDF5-GOY"):
-					val = data[jj][d]
-
-				# Checking mode
-				if (dimension_mode == "log"):
-					val = math.log10(val)
-				
-				# Remap
-				min_val = minmaxs[d][0]
-				max_val = minmaxs[d][1]
-				min_target = 0
-				max_target = lq_max if (dimension_quality == "LQ") else hq_max
-				val = int(round_to_n(remap(val, min_val, max_val, min_target, max_target, True), digits + 1))
-				
-				# Feed row to later write to file
-				if (is_dimension_kept):
-					if (d > 0):
-						row = row + " "
-					
-					if (not zoombox or (zoombox and is_in_box)):
-						row = row + str(val)
-						
-			# Log row sometimes
-			if (j % max(1, int(round(actual_count/nb_logs))) == 0):
-				if (zoombox and not is_in_box):
-					logger.bind(color="fg #3FB").trace(str(j) + "th remapped row is: out of zoombox")
-				else:
-					logger.bind(color="fg #3FB").trace(str(j) + "th remapped row is: " + row.lstrip('\n'))
-
-			# Write to file
-			if (not zoombox or (zoombox and is_in_box)):
-				destination_file.write(row)
-				output_rows_count += 1
-				is_first_line_written = False
-
-		# Log export time
-		end_time = datetime.datetime.now()
-		delta = end_time.timestamp() - start_time.timestamp()
-		logger.success("Exported data in: " + str(round(delta, 2)) + " seconds.")
-
-		# Conclude
-		logger.success("File " + dest_file_name + ".txt with " + str(output_rows_count) + " rows was created.")
+		
